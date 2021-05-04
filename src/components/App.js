3e771f8f6e6ca4b2375fa911308f6901f25c842c
Jędrek Dąbrowski <jedrzejdabrowski.jd@gmail.com>
2021-05-04 14:36:17 +0200

    media Queries

--- src/components/App.js
+++ src/components/App.js
@@ -32,7 +32,7 @@ class App extends Component {
 
 handleCitySubmit = (e) =>{
   e.preventDefault();
-  const API =`http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIkey}&units=metric`;
+  const API =`https://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIkey}&units=metric`;
 
   fetch(API)
   .then(response => {
